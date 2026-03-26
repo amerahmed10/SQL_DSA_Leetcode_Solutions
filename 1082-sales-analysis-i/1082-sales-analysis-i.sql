4270e63db8cbf0d26b9cebed5f65e599e1bcfecf
amerahmed10 <79590363+amerahmed10@users.noreply.github.com>
2026-03-26 09:27:43 -0500

    Time: 1047 ms (35.24%), Space: 0B (100%) - LeetHub

--- 1082-sales-analysis-i/1082-sales-analysis-i.sql
+++ 1082-sales-analysis-i/1082-sales-analysis-i.sql
@@ -1,10 +1,5 @@
 # Write your MySQL query statement below
-with total_added as (select seller_id, sum(price) as total
-from Sales
-group by seller_id),
-
-ranked as (select seller_id , dense_rank() over(order by total desc) as rk from total_added)
-
 select seller_id
-from ranked
-where rk=1
+from Sales
+group by seller_id
+having sum(price)>= all (select sum(price) from Sales group by seller_id)
